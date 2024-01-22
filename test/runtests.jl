be6ccda37ba6b593ea03cbef0c32481decd1050c
Tim Holy <tim.holy@gmail.com>
2024-01-22 16:48:54 -0600

    Aqua 0.8, bound in [compat]
    Fixes CI failures

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Aqua, Documenter
 
 @testset "Project meta quality checks" begin
     # Not checking compat section for test-only dependencies
-    Aqua.test_all(StackViews; project_extras=true, deps_compat=true, stale_deps=true, project_toml_formatting=true)
+    Aqua.test_all(StackViews; project_extras=true, deps_compat=true, stale_deps=true)
     if VERSION >= v"1.2"
         doctest(StackViews, manual = false)
     end
